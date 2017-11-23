a4533e1c01ae60b737cfa5c11b555623f1f4ae76
andre.fernandes <andrefernades3@gmail.com>
2017-11-23 17:26:10 -0200

    comentado

--- app/src/main/java/andre/usjt/br/meuprimeiroappsi3anbua/ExibirMensagemActivity.java
+++ app/src/main/java/andre/usjt/br/meuprimeiroappsi3anbua/ExibirMensagemActivity.java
@@ -1,16 +1,25 @@
 package andre.usjt.br.meuprimeiroappsi3anbua;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ExibirMensagemActivity extends AppCompatActivity {
+public class ExibirMensagemActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exibir_mensagem);
-        Intent i = getIntent();
-        Bundle b = i.getExtras();
-        String texto =
+        //obtenho a intent do metodo herdado
+        Intent x = getIntent();
+        //pego o bundle e coloco como parametro uma chave TXT
+        String texto = x.getStringExtra(MainActivity.TXT);
+        TextView textView = new TextView(this);
+        //textView.setTextSize(100);
+        textView.setText(texto);
+        ViewGroup layout = (ViewGroup)findViewById(R.id.activity_exibir_mensagem);
+        layout.addView(textView);
     }
 }
--- app/src/main/java/andre/usjt/br/meuprimeiroappsi3anbua/MainActivity.java
+++ app/src/main/java/andre/usjt/br/meuprimeiroappsi3anbua/MainActivity.java
@@ -1,26 +1,36 @@
 package andre.usjt.br.meuprimeiroappsi3anbua;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private EditText textoEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textoEditText = (EditText) findViewById(R.id.textoEditText);
-
     }
 
     public void enviarMensagem( View v){
+        //busca o elemento e faz un casting
+        textoEditText = (EditText) findViewById(R.id.textoEditText);
+        //obtenho o elemento na String toString uso para converter
         String texto = textoEditText.getText().toString();
+        //coloco os objetos numa sacola
         Bundle b = new Bundle();
-        b.putString("txt", texto);
-        Intent i = new Intent (this, ExibirMensagemActivity.class);
-        i.putExtras(b);
-        startActivity(i);
+        //aqui coloco pares valor ; (chave, valor)
+        b.putString(TXT, texto);
+        //dizer a intenção que tambem é um objeto
+        Intent intencao = new Intent (this, ExibirMensagemActivity.class);
+        //aqui coloco dados extras dentro do intent
+        intencao.putExtras(b);
+        //inicio outra activity
+        startActivity(intencao);
     }
+    public static final String TXT = "";
 }
